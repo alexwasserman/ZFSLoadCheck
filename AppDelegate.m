ab2e1b8941710a21cb33989e3b571ed96869ebae
Alex Wasserman <alex@alexwasserman.com>
2017-02-10 03:14:21 -0500

    Updated to use /Users/.zfsloadcheck file to check

--- AppDelegate.m
+++ AppDelegate.m
@@ -1,7 +1,10 @@
 #import "AppDelegate.h"
+#import "LogManager.h"
 
 @interface AppDelegate () <NSApplicationDelegate>
+
 @property (nonatomic, assign, readwrite) IBOutlet NSPanel *     panel;
+
 @end
 
 @implementation AppDelegate
@@ -9,16 +12,15 @@
 
 - (void)awakeFromNib
 {
-    self.panel.level=2147483631;
     NSLog(@"Checking ZFS");
     zfsStatus.stringValue = @"Checking ZFS";
     [NSTimer scheduledTimerWithTimeInterval:5.0f
-    target:self selector:@selector(updateStatus:) userInfo:nil repeats:YES];
+                                     target:self selector:@selector(updateStatus:) userInfo:nil repeats:YES];
 }
 
 - (void)updateStatus:(NSTimer *)timer
 {
-    NSString *pathToFile = @"/Users/alex/.zfsTest";
+    NSString *pathToFile = @"/Users/.zfsloadcheck";
     BOOL isDir = NO;
     BOOL isFile = [[NSFileManager defaultManager] fileExistsAtPath:pathToFile isDirectory:&isDir];
     
@@ -27,43 +29,52 @@
         NSLog(@"ZFS Mounted");
         zfsStatus.stringValue = @"ZFS Mounted";
     }
-        else
+    else
     {
         NSLog(@"ZFS NOT Mounted");
         zfsStatus.stringValue = @"ZFS Not Mounted!";
     }
 }
 
-- (IBAction)simpleButtonPressed:(id)sender {
-    [NSApp terminate:self];
-}
-
 
 - (void)applicationDidFinishLaunching:(NSNotification *)note
 {
-#pragma unused(note)
+    #pragma unused(note)
     
+    [[LogManager sharedManager] logWithFormat:@"Did finish launching begin"];
+
     assert(self.panel != nil);
+
+    // We have to call -[NSWindow setCanBecomeVisibleWithoutLogin:] to let the 
+    // system know that we're not accidentally trying to display a window 
+    // pre-login.
     
     [self.panel setCanBecomeVisibleWithoutLogin:YES];
+    
+    // Our application is a UI element which never activates, so we want our 
+    // panel to show regardless.
+    
     [self.panel setHidesOnDeactivate:NO];
 
+    // Due to a problem with the relationship between the UI frameworks and the 
+    // window server <rdar://problem/5136400>, -[NSWindow orderFront:] is not 
+    // sufficient to show the window.  We have to use -[NSWindow orderFrontRegardless].
+
     if ([[NSUserDefaults standardUserDefaults] boolForKey:@"ForceOrderFront"]) {
+        [[LogManager sharedManager] logWithFormat:@"Showing window with extreme prejudice"];
         [self.panel orderFrontRegardless];
     } else {
+        [[LogManager sharedManager] logWithFormat:@"Showing window normally"];
         [self.panel orderFront:self];
-        [self.panel orderFrontRegardless];
     }
-    
-    self.panel.level=2147483631;
-    
-    }
+
+    [[LogManager sharedManager] logWithFormat:@"Did finish launching end"];
+}
 
 - (void)applicationWillTerminate:(NSNotification *)note
 {
-#pragma unused(note)
-    NSLog(@"Quitting");
-
+    #pragma unused(note)
+    [[LogManager sharedManager] logWithFormat:@"Will terminate"];
 }
 
 @end
--- main.m
+++ main.m
@@ -1,7 +1,15 @@
 #import <Cocoa/Cocoa.h>
 
+#import "LogManager.h"
+
 static void WaitForWindowServerSession(void)
+    // This routine waits for the window server to register its per-session 
+    // services in our session.  This code was necessary in various pre-release 
+    // versions of Mac OS X 10.5, but it is not necessary on the final version. 
+    // However, I've left it in, and the option to enable it, to give me the 
+    // flexibility to test this edge case.
 {
+    [[LogManager sharedManager] logWithFormat:@"Waiting for window server session begin"];
     
     do {
         NSDictionary *  sessionDict;
@@ -10,12 +18,16 @@ static void WaitForWindowServerSession(void)
         if (sessionDict != nil) {
             break;
         }
+        [[LogManager sharedManager] logWithFormat:@"No window server session, wait"];
         sleep(1);
     } while (YES);
 
+    [[LogManager sharedManager] logWithFormat:@"Waiting for window server session end"];
 }
 
 static void InstallHandleSIGTERMFromRunLoop(void)
+    // This routine installs a SIGTERM handler that's called on the main thread, allowing 
+    // it to then call into Cocoa to quit the app.
 {
     static dispatch_once_t   sOnceToken;
     static dispatch_source_t sSignalSource;
@@ -29,6 +41,8 @@ static void InstallHandleSIGTERMFromRunLoop(void)
         dispatch_source_set_event_handler(sSignalSource, ^{
             assert([NSThread isMainThread]);
             
+            [[LogManager sharedManager] logWithFormat:@"Got SIGTERM"];
+
             [[NSApplication sharedApplication] terminate:nil];
         });
         
@@ -41,6 +55,10 @@ int main(int argc, char *argv[])
     int             retVal;
     NSTimeInterval  delay;
     
+    [[LogManager sharedManager] logWithFormat:@"Start"];
+    
+    // Register the default defaults, so to speak.
+    
     [[NSUserDefaults standardUserDefaults] registerDefaults:@{
         @"DelayStartup":               @0.0, 
         @"WaitForWindowServerSession": @NO,
@@ -52,19 +70,32 @@ int main(int argc, char *argv[])
     
     if ([[NSUserDefaults standardUserDefaults] boolForKey:@"WaitForWindowServerSession"]) {
         WaitForWindowServerSession();
+    } else {
+        [[LogManager sharedManager] logWithFormat:@"Not waiting for CGSessionCopyCurrentDictionary"];
     }
     
     delay = [[NSUserDefaults standardUserDefaults] doubleForKey:@"DelayStartup"];
     if (delay > 0.0) {
+        [[LogManager sharedManager] logWithFormat:@"Delaying %.1f", delay];
         [NSThread sleepForTimeInterval:delay];
+    } else {
+        [[LogManager sharedManager] logWithFormat:@"Not delaying"];
     }
     
     if ([[NSUserDefaults standardUserDefaults] boolForKey:@"CleanExit"]) {
+        [[LogManager sharedManager] logWithFormat:@"Installing SIGTERM handler"];
         InstallHandleSIGTERMFromRunLoop();
+    } else {
+        [[LogManager sharedManager] logWithFormat:@"Not installing SIGTERM handler"];
     }
     
+    // Go go gadget Cocoa!
+    
+    [[LogManager sharedManager] logWithFormat:@"Starting Cocoa application"];
+
     retVal = NSApplicationMain(argc, (const char **) argv);
+
+    [[LogManager sharedManager] logWithFormat:@"Cocoa application returned!?"];
     
     return retVal;
-    
 }
